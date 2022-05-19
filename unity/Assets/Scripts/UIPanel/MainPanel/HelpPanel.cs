6372e592793c8cc322b839999af9c91f21c86498
zhubowen <autism41602@qq.com>
2022-05-19 15:58:24 +0800

    上一次未提交

--- unity/Assets/Scripts/UIPanel/MainPanel/HelpPanel.cs
+++ unity/Assets/Scripts/UIPanel/MainPanel/HelpPanel.cs
@@ -25,12 +25,16 @@ public class HelpPanel : BasePanel
         list.SetVirtualAndLoop();
         list.itemRenderer = RenderListItem;
         list.numItems = 7;
-        list.scrollPane.onScroll.Set(() => { DoSpecialEffect(); });
+        // list.scrollPane.onScroll.Set(() => { DoSpecialEffect(); });
+        list.scrollPane.onScroll.Add(this.DoSpecialEffect);
+        DoSpecialEffect();
+        ShowIDInformation(currentCardID);
+        // list.scrollPane.onScroll.Set(this.DoSpecialEffect);
     }
 
     private void DoSpecialEffect()
     {
-        float listCenter = list.scrollPane.posX + list.viewWidth / 2;
+        float listCenter = list.scrollPane.posX + list.viewWidth / 2; //中心x坐标
 
         //list.numChildren 显示在屏幕上的对象
         for (int i = 0; i < list.numChildren; i++)
@@ -49,6 +53,8 @@ public class HelpPanel : BasePanel
                 item.SetScale(1, 1);
             }
         }
+
+        ShowIDInformation((list.GetFirstChildInView() + 3) % list.numItems);
     }
 
     private void RenderListItem(int index, GObject gObject)
@@ -56,5 +62,45 @@ public class HelpPanel : BasePanel
         GButton button = gObject.asButton;
         button.SetPivot(0.5f, 0.5f);
         button.icon = UIPackage.GetItemURL("Res_Main", "ID" + (index + 1));
+    }
+
+    //文本显示控制
+    private void ShowIDInformation(int id)
+    {
+        switch (id)
+        {
+            case 1:
+                idText.text = "狼人";
+                idIntroduction.text = "每晚可以杀死一名玩家，狼队互相认识";
+                break;
+            case 2:
+                idText.text = "预言家";
+                idIntroduction.text = "每晚可以确认一名玩家的真实身份，只能验到狼跟非狼";
+                break;
+            case 3:
+                idText.text = "女巫";
+                idIntroduction.text = "有两种药（解锁和毒药），解药可以救人，毒药可以在天黑是毒死一人，每种药只能使用一次" +
+                                      "，且同一晚不能使用两种药，女巫解锁使用前可以看到谁被指杀";
+                break;
+            case 4:
+                idText.text = "猎人";
+                idIntroduction.text = "当他被狼人杀掉且夜晚没有被女巫毒杀或者被放逐投票出局，可以选择一名活着的玩家一起出局";
+                break;
+            case 5:
+                idText.text = "守卫";
+                idIntroduction.text = "每晚可以守护一名玩家（包括自己），被守护玩家不能被狼人杀死，但可以被女巫毒死，守护不可以两天" +
+                                      "连续守护同一名玩家，要隔一天才能守护";
+                break;
+            case 6:
+                idText.text = "村民";
+                idIntroduction.text = "没有任何特殊功能";
+                break;
+            case 0:
+                idText.text = "警长";
+                idIntroduction.text = "可以决定发言顺序并且有1.5票放逐投票权";
+                break;
+            default:
+                break;
+        }
     }
 }
